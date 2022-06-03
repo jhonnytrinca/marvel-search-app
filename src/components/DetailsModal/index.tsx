b53e7ca7d159e3473df68dedefa675b52bd385f4
Jhonny Trinca <jhonny.trinca@hotmail.com>
2022-06-03 19:50:42 -0300

    feat: modal info

--- src/components/DetailsModal/index.tsx
+++ src/components/DetailsModal/index.tsx
@@ -3,73 +3,134 @@ import useComicDetails from '../../hooks/useComicDetails';
 import useOnKeyDown from '../../hooks/useOnKeyDown';
 import ComicsService from '../../services/ComicsService';
 import { Button } from '../Button';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { BiMessageAltDetail } from 'react-icons/bi';
 
 export const DetailsModal = ({ comic, closeModal }: any) => {
-  const { handleDate, handlePrice, handleDetails } = useComicDetails(comic);
-  const { data } = useSWR(
+  const { handleDate, handlePrice, handleDetails, handleCreators } =
+    useComicDetails(comic);
+  const { data, isValidating } = useSWR(
     comic?.characters?.collectionURI,
     ComicsService.getCharacters
   );
   const characters = data?.data?.data.results;
   useOnKeyDown('Escape', closeModal);
 
+  console.log(comic);
+
   return (
-    <div className='absolute z-10 flex justify-center items-center'>
-      <div className='w-[40rem] h-[40rem] border-2 bg-white'>
-        <>
-          {console.log(comic)}
-          <Button onClick={closeModal}>Fechar</Button>
-          <div>
-            <img
-              src={`${comic.thumbnail?.path}/standard_fantastic.${comic?.thumbnail?.extension}`}
-              alt={comic.title}
-              className='w-16 h-16'
-            />
-            <span className='font-semibold'>{comic?.title}</span>
-            <span>{comic?.description || 'Descrição não disponível'}</span>
-            <span>detalhes: {handleDetails()}</span>
-            <div>
-              <span className='font-semibold'>Número de páginas: </span>
-              <span>{comic?.pageCount || 'Não disponível'}</span>
-            </div>
-            {comic?.creators?.items.length > 0 && (
-              <div>
-                <span className='font-semibold'>Criadores: </span>
-                {comic?.creators?.items.map((creator: any) => (
-                  <span className='capitalize'>
-                    {creator.name} - {creator.role};{' '}
-                  </span>
-                ))}
+    <div className='fixed top-0 left-0 w-full h-screen z-10 flex justify-center items-center bg-black/40'>
+      <div className='w-4/5 h-10/12 rounded-xl bg-white relative px-8 py-3'>
+        <div className='absolute -top-4 -left-20 rounded-lg shadow-lg w-[16rem] h-[23rem]'>
+          <img
+            src={`${comic.thumbnail?.path}/detail.${comic?.thumbnail?.extension}`}
+            alt={comic.title}
+            className='rounded-lg object-cover w-full h-full'
+          />
+        </div>
+        <div className='w-full flex flex-col'>
+          <div className='flex flex-col w-10/12 self-end '>
+            <div className='h-[14rem] min-h-fit'>
+              <h1 className='font-semibold text-3xl text-center uppercase'>
+                {comic?.title}
+              </h1>
+              <div className='max-h-[11rem] overflow-auto py-3 text-sm'>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: comic?.description || 'Descrição não informada'
+                  }}
+                  className='beforeBar italic'
+                />
               </div>
-            )}
-            <div>
-              <span className='font-semibold'>Data de lançamento: </span>
-              <span>{handleDate()}</span>
+              <p className='flex items-center text-red-600 font-semibold py-2 text-sm'>
+                <BiMessageAltDetail size={20} />
+                <span>
+                  <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href={handleDetails()}
+                    className='underline pl-2'
+                  >
+                    Clique aqui
+                  </a>{' '}
+                  para mais detalhes desse quadrinho
+                </span>
+              </p>
             </div>
-            <div>
-              <span className='font-semibold'>Preço de lançamento: </span>
-              <span>{handlePrice()}</span>
-            </div>
-            <div>
-              <span className='font-semibold'>Série a que faz parte: </span>
-              <span>{comic?.series?.name || 'Não disponível'}</span>
+            <div className=' mt-5 leading-8 max-h-[26rem] overflow-auto'>
+              <div>
+                <span className='topics'>Série a que faz parte: </span>
+                <span>{comic?.series?.name || 'Não disponível'}</span>
+              </div>
+              <div className='grid grid-cols-3'>
+                <div>
+                  <span className='topics'>Número de páginas: </span>
+                  <span>{comic?.pageCount || 'Não disponível'}</span>
+                </div>
+                <div>
+                  <span className='topics'>Data de lançamento: </span>
+                  <span>{handleDate()}</span>
+                </div>
+                <div>
+                  <span className='topics'>Preço de lançamento: </span>
+                  <span>{handlePrice()}</span>
+                </div>
+                {comic?.creators?.items.length > 0 && (
+                  <>
+                    <div>
+                      <span className='topics'>Escritores: </span>
+                      <span>{handleCreators('writers')}</span>
+                    </div>
+                    <div>
+                      <span className='topics'>Desenhistas: </span>
+                      <span>{handleCreators('pencillers')}</span>
+                    </div>
+                    <div>
+                      <span className='topics'>Arte da capa: </span>
+                      <span>{handleCreators('covers')}</span>
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-          {characters?.length > 0 && (
-            <div className='flex'>
-              {characters.map((character: any) => (
-                <div className='flex flex-col flex-wrap gap-2 items-center '>
-                  <img
-                    src={`${character.thumbnail?.path}/standard_fantastic.${comic?.thumbnail?.extension}`}
-                    alt={character.name}
-                    className='w-16 h-16'
-                  />
-                  <span>{character.name}</span>
-                </div>
-              ))}
-            </div>
-          )}
-        </>
+          <div className='min-h-[9rem]'>
+            {isValidating ? (
+              'Loading component'
+            ) : (
+              <>
+                {characters?.length > 0 && (
+                  <div className='mt-2'>
+                    <span className='topics'>Personagens participantes:</span>
+                    <div className='flex gap-4 overflow-x-auto mt-2'>
+                      {characters.map((character: any) => (
+                        <div className='flex flex-col flex-wrap gap-2 items-center mx-2 w-full'>
+                          <div className='w-20 rounded-full'>
+                            <img
+                              src={`${character.thumbnail?.path}/standard_fantastic.${comic?.thumbnail?.extension}`}
+                              alt={character.name}
+                              className='rounded-full'
+                            />
+                          </div>
+                          <span className='uppercase font-bold text-center text-sm	'>
+                            {character.name}
+                          </span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+
+        <Button
+          onClick={closeModal}
+          className='absolute -top-7 -right-7 w-16 h-16 rounded-full bg-red-600 flex items-center justify-center '
+        >
+          <RiCloseCircleLine size={60} className='text-white' />
+        </Button>
       </div>
     </div>
   );
--- src/hooks/useComicDetails.tsx
+++ src/hooks/useComicDetails.tsx
@@ -27,7 +27,50 @@ const useComicDetails = (comic: any) => {
     return detail.url;
   };
 
-  return { handleDate, handlePrice, handleDetails };
+  const handleCreators = (type: string) => {
+    const writers = comic.creators?.items?.filter(
+      (comic: any) => comic.role === 'writer'
+    );
+    const pencillers = comic.creators?.items.filter(
+      (comic: any) => comic.role === 'penciller'
+    );
+    const covers = comic.creators?.items.filter(
+      (comic: any) => comic.role === 'penciller (cover)'
+    );
+
+    console.log(covers);
+
+    if (type === 'writers') {
+      return (
+        <span>
+          {writers.length > 0 &&
+            `${writers[0].name}${
+              writers.length > 1 ? ` + ${writers.length - 1}` : ''
+            };`}
+        </span>
+      );
+    } else if (type === 'pencillers') {
+      return (
+        <span>
+          {pencillers.length > 0 &&
+            `${pencillers[0].name}${
+              pencillers.length > 1 ? ` + ${pencillers.length - 1}` : ''
+            };`}
+        </span>
+      );
+    } else if (type === 'covers') {
+      return (
+        <span>
+          {covers.length > 0 &&
+            `${covers[0].name}${
+              covers.length > 1 ? `+ ${covers.length - 1}` : ''
+            };`}
+        </span>
+      );
+    }
+  };
+
+  return { handleDate, handlePrice, handleDetails, handleCreators };
 };
 
 export default useComicDetails;
