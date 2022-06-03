04870034be0f3d38fcb0cbe08fdc9c581e20013b
Jhonny Trinca <jhonny.trinca@hotmail.com>
2022-06-03 12:16:31 -0300

    feat: call data on home page

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,5 @@ export { Header } from './Header';
 export { Button } from './Button';
 export { Checkbox } from './Checkbox';
 export { Input } from './Input';
+export { Toast } from './Toast';
+export { Card } from './Card';
