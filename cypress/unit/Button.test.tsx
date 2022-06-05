404dad86061f77d680c5b75e400c0b501d4a60db
Jhonny Trinca <jhonny.trinca@hotmail.com>
2022-06-05 19:47:15 -0300

    Updates

--- cypress/unit/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import {mount} from 'cypress/react'
-import {Button} from '../../src/components'
-import "../../node_modules/tailwindcss/tailwind.css"
-
-it('should render correctly', () => {
-  mount (<Button>Clique aqui</Button>);
-  cy.findByRole('button').click();
-})
--- src/pages/Home/validation.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup'
-
-export const schemaDefault = yup.object().shape({
-  selectedOptions: yup.array(),
-  name: yup.string().required('É necessário informar um nome'),
-  email: yup.string().email('Informe um email válido').required('É necessário informar um email')
-}) 
