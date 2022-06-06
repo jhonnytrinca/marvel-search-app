6e55e6a55f33602668ef42cd14bbb238180774b5
Jhonny Trinca <jhonny.trinca@hotmail.com>
2022-06-06 11:39:22 -0300

    Updates

--- cypress/unit/Loading.test.tsx
+++
@@ -1,9 +0,0 @@
-import { mount } from 'cypress/react';
-import { Loading } from '../../src/components';
-import '../../node_modules/tailwindcss/tailwind.css';
-
-it('should render correctly', () => {
-  mount(<Loading />);
-  cy.findByRole('img');
-  cy.findByText('Carregando...');
-});
